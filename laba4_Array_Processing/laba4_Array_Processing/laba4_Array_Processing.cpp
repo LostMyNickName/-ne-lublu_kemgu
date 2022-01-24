ab052c47bf75f48e09945e5e88b662aa5a744a6d
LostMyNickName <lukdiman233@gmail.com>
2022-01-24 11:13:26 +0700

    laba4
    обработка массивов готово

---
+++ laba4_Array_Processing/laba4_Array_Processing/laba4_Array_Processing.cpp
@@ -0,0 +1,51 @@
+﻿/****************************
+* Лукьянов Дмитрий ПИз-211  *
+* Обработка массивов        *
+* Вариант 5                 *
+* Codeguid: -               *
+*****************************/
+
+#include <iostream>
+#include <clocale>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "RUS");
+	srand(time(0));
+
+	const int col = 7, row = 5;
+	int F[col][row];
+
+	for (int i = 0; i < col; ++i) {
+		for (int j = 0; j < row; ++j) {
+			F[i][j] = 100 - rand() % 200;
+		}
+	}
+
+	cout << "Исходная матрица:\n";
+
+	for (int i = 0; i < col; ++i) {
+		for (int j = 0; j < row; ++j) {
+			cout <<setw(5) << F[i][j];
+		}
+		cout << endl;
+	}
+
+	cout << "Измененная матрица:\n";
+
+	for (int i = 0; i < col; ++i) {
+		for (int j = 0; j < row; ++j) {
+			if (F[i][j] < 0) //да ну религия не позволяет ставить фигурную скобку, когда можно не ставить
+				F[i][j] = 0;
+
+			cout << setw(5) << F[i][j];
+		}
+		cout << endl;
+	}
+}
+
